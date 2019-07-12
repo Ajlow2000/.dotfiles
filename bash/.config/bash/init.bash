a9a82c86080d6b2955270fdd98d5e6b96b534562
Ajlow2000 <ajlow2000@gmail.com>
2019-07-11 22:42:12 -0500

    Fixed a bug when adding /home/ajlow/.local/bin* to PATH

--- bash/.config/bash/init.bash
+++ bash/.config/bash/init.bash
@@ -1,7 +1,8 @@
-# print TODO.sh upon launch
-TODO.sh
-
 # Add all directories in $HOME/.local/bin/ to PATH
+PATH+=":$HOME/.local/bin/"
 for d in $HOME/.local/bin/*; do
 	PATH+=":$d"
 done
+
+# print TODO.sh upon launch
+$HOME/.local/bin/TODO.sh
