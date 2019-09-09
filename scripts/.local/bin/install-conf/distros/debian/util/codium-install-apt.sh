f0224bc34a054b1e0179544923ade748b795b623
Ajlow2000 <ajlow2000@gmail.com>
2019-09-09 14:44:00 -0500

    Scripts

--- scripts/.local/bin/install-conf/distros/debian/util/codium-install-apt.sh
+++ scripts/.local/bin/install-conf/distros/debian/util/codium-install-apt.sh
@@ -2,6 +2,9 @@
 
 # This script installs VS Codium, the FOSS variant of VS Code.  Only works on DEB
 
+# Deps
+sudo apt install wget
+
 # Add the duuds key
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
 
---
+++ scripts/.local/bin/wiscTODO.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script serves as a todo list for my academic stuff
+
+
+echo "-----WiscTODO-----"
+echo ""
+echo "PS170"
+echo "  * Sept26th -  Identify TWO research questions and compose a THEORY"
+echo "  * One textbook"
+echo "  * Register for TopHat"
+echo ""
+echo "Math234"
+echo "  * Cengage textbook"
+echo ""
+echo "CS240"
+echo ""
+echo "ECE252"
+echo ""
