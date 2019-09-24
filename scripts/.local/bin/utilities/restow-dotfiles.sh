e7072c89aad1619de4bf698cb994e13033b45de0
Ajlow2000 <Ajlow2000@gmail.com>
2019-09-23 20:32:04 -0500

    Spelling error

--- scripts/.local/bin/utilities/restow-dotfiles.sh
+++ scripts/.local/bin/utilities/restow-dotfiles.sh
@@ -7,7 +7,7 @@ githubLink=https://github.com/Ajlow2000/.dotfiles.git
 # Wipe and reclone
 rm -rf $HOME/.dotfiles
 cd $HOME
-git clone $githubLInk
+git clone $githubLink
 
 # Stow new dotfiles
 for d in $HOME/.dotfiles/*; do
