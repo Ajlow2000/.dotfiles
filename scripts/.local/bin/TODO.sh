1691771469a75f7bbb65b474c08324ee6ea6367c
Ajlow2000 <Ajlow2000@gmail.com>
2019-09-23 19:10:14 -0500

    Setting up scripts

--- scripts/.local/bin/TODO.sh
+++ scripts/.local/bin/TODO.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "############################### TODO ####################################"
-echo "- Server: Finish assembling and Install Nextcloud"
-echo "- config-unix: re structure.  lol"
-echo "- Configure compton and conky and rofi"
-echo "- Configure login manager and maybe i3lock"
+echo "- Set up plex"
+echo "- Set up nextcloud"
+echo "- Learn cron jobs: automate the restart of docker containers on reboot"
 echo "#########################################################################"
