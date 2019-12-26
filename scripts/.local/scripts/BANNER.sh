1b0539d7f9a13b77670f996ba678b0d575deb8f8
Ajlow2000 <ajlow2000@gmail.com>
2019-12-25 18:10:50 -0600

    BANNER

--- scripts/.local/scripts/BANNER.sh
+++ scripts/.local/scripts/BANNER.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "################################################"
-echo "# i3"
-echo "#   - Transparency (compton)"
+echo "# BSPWM/SXHKD"
+echo "#   - Install and configure"
 echo "#   - Polybar"
 echo "#   - Smooth out network, volume and brightness"
-echo "#   - Rofi-dmenu"
+echo "# NEtowrk-manager
 echo "# Tmux"
 echo "# Snapd"
 echo "################################################"
